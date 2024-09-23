0f9d2c3fb2a9e87d9a8ebac048f5b7d453d6446f
Patricija Brečko <patricija@danesjenovdan.si>
2024-09-23 14:56:38 +0200

    project setup

---
+++ acf/acf/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-(p^87xi4(mqzbalf&!=ie-i5j=a4l_oj@#4#!i1)*rzn2z^bru"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
