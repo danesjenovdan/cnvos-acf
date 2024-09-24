0be93e1e4e50efb5c8cd9aa4490c8598ac644c6a
Patricija Brečko <patricija@danesjenovdan.si>
2024-09-24 13:19:52 +0200

    fix staticfiles storage in settings

--- acf/acf/settings/base.py
+++ acf/acf/settings/base.py
@@ -148,18 +148,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
 
 # Default storage settings, with the staticfiles storage updated.
 # See https://docs.djangoproject.com/en/4.2/ref/settings/#std-setting-STORAGES
-STORAGES = {
-    "default": {
-        "BACKEND": "django.core.files.storage.FileSystemStorage",
-    },
-    # ManifestStaticFilesStorage is recommended in production, to prevent
-    # outdated JavaScript / CSS assets being served from cache
-    # (e.g. after a Wagtail upgrade).
-    # See https://docs.djangoproject.com/en/4.2/ref/contrib/staticfiles/#manifeststaticfilesstorage
-    "staticfiles": {
-        "BACKEND": "django.contrib.staticfiles.storage.ManifestStaticFilesStorage",
-    },
-}
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
 
 
 # Wagtail settings
